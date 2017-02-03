a3e2ba698cc080718624d1ba0ff9a700bccd4674
xuchao <85825770@qq.com>
2017-02-03 11:56:26 +0800

    修正

--- HighlightJsAsset.php
+++ HighlightJsAsset.php
@@ -6,6 +6,7 @@
  */
 namespace xutl\highlightjs;
 
+use yii\web\View;
 use yii\web\AssetBundle;
 
 class HighlightJsAsset extends AssetBundle
@@ -14,15 +15,51 @@ class HighlightJsAsset extends AssetBundle
      * @inherit
      */
     public $sourcePath = '@xutl/highlightjs/assets';
+    //public $sourcePath = '';
+
+    //public $basePath = '//cdn.bootcss.com/highlight.js/9.9.0';
 
     public $css = [
-        'styles/default.css'
+        'styles/default.min.css'
     ];
 
     /**
      * @inherit
      */
     public $js = [
-        'highlight.js',
+        'highlight.min.js',
+
     ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+    public static $language = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (!is_null(static::$language)) {
+            array_push($this->js, static::$language);
+            //array_unshift($this->js, static::$language);
+        }
+    }
+
+    /**
+     * Registers this asset bundle with a view.
+     * @param \yii\base\View $view the view to be registered with
+     * @param null|string $language
+     * @return static the registered asset bundle instance
+     */
+    public static function register($view, $language = null)
+    {
+        if (!is_null($language)) {
+            static::$language = 'languages/' . $language . '.min.js';
+        }
+        return $view->registerAssetBundle(get_called_class());
+    }
 }
